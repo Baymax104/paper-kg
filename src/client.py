6b2b02b2b0827b1a510b9d706d885d0401a87ed2
John Wei <wzy1048168235@163.com>
2025-01-07 19:35:12 +0800

    feat(main): 完善多线程调用模型API

--- src/client.py
+++ src/client.py
@@ -1,40 +1,42 @@
 # -*- coding: UTF-8 -*-
+from typing import TypeAlias
+
 from zhipuai import ZhipuAI
 from concurrent import futures
 from loguru import logger
 from tqdm import tqdm
 import time
 
+Sample: TypeAlias = dict[str, str]
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        logger.info(f"Start {func.__name__}")
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        logger.info(f"End {func.__name__} spending {(end - start):2f} seconds")
+        return result
+    return wrapper
+
+
 class Client:
 
-    def __init__(self):
-        self.client = ZhipuAI(api_key='e10a6f5e200c866e7b2e95b28be44fe3.48jeVvIcKr7pplj1')
-        self.batch_size = 2
-        self.max_workers = 10
-        self.prompt = '''
-        你好，你是一个实体和关系提取的专家，你需要从以下给定的文本中提取出命名实体以及它们的关系，并严格按照给定的json格式输出
-        要求：提取的命名实体类型尽量分为研究问题(Problem)、研究方法(Method)、实验结果(Result)、结论(Conclusion)，对于同义关系，应该有相同的名称
-        输入：#{0}#
-        输出：
-        {{
-            'entities': [
-                {{'id': '', 'type': '', 'name': ''}},
-            ],
-            'relations': [
-                {{'type': '', 'from_entity_id': '', 'to_entity_id': ''}}
-            ]
-        }}
-        '''
+    def __init__(self, batch_size=2, max_workers=10):
+        self.client = ZhipuAI(api_key='ba802c1855ebdba8235bf095766f104e.oAwCWXo3QFwHuAi7')
+        self.batch_size = batch_size
+        self.max_workers = max_workers
 
-
-    def extract(self, texts: list[str]) -> list[str]:
+    @timer
+    def extract(self, texts: list[Sample]) -> list[Sample]:
         results = []
         texts = [texts[i:i + self.batch_size] for i in range(0, len(texts), self.batch_size)]
         processing_bars = []
         tasks = {}
         with futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             for i, batch in enumerate(texts):
-                pbar = tqdm(total=self.batch_size, desc=f'Batch-{i + 1}', position=i)
+                pbar = tqdm(total=len(batch), desc=f'Batch-{i + 1}', position=i, dynamic_ncols=True)
                 processing_bars.append(pbar)
                 future = executor.submit(self.__chat, batch, pbar)
                 tasks[future] = (batch, pbar)
@@ -50,19 +52,17 @@ class Client:
 
         return results
 
-    def __chat(self, batch: list[str], pbar) -> list[dict[str, str]]:
+
+    def __chat(self, batch: list[Sample], pbar) -> list[dict[str, str]]:
         task_texts: dict[str, str] = {}
-        for text in batch:
-            task = self.client.chat.asyncCompletions.create(
-                model='GLM-4-Flash',
-                messages=[{'role': 'user', 'content': self.prompt.format(text)}]
-            )
-            task_texts[task.id] = text
+        for sample in batch:
+            task = self.client.chat.asyncCompletions.create(model='GLM-4-Flash', messages=sample['messages'])
+            task_texts[task.id] = sample['id']
 
         results = []
         processing_ids = set(task_texts.keys())
         while len(processing_ids) > 0:
-            for task_id, text in task_texts.items():
+            for task_id, id_ in task_texts.items():
                 if task_id not in processing_ids:
                     continue
                 response = self.client.chat.asyncCompletions.retrieve_completion_result(task_id)
@@ -70,7 +70,14 @@ class Client:
                     processing_ids.remove(task_id)
                     pbar.update(1)
                     if response.task_status == 'SUCCESS':
-                        results.append({text: response.choices[0].message.content})
+                        results.append({
+                            'id': id_,
+                            'content': response.choices[0].message.content
+                        })
                 time.sleep(2)
 
         return results
+
+
+    def __batch_process(self, batch: list[Sample], pbar) -> list[dict[str, str]]:
+        pass
