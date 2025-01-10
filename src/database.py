547fdad8a4bb0247b2df2697a13e425ecba29c18
John Wei <wzy1048168235@163.com>
2025-01-10 23:07:36 +0800

    feat(main): 完善多线程插入neo4j数据库

---
+++ src/database.py
@@ -0,0 +1,91 @@
+# -*- coding: UTF-8 -*-
+import json
+import threading
+from concurrent import futures
+from typing import Any
+
+import loguru
+from py2neo import Graph, Node, Relationship
+from tqdm import tqdm
+
+
+class Database:
+
+    def __init__(self, batch_size=1000, max_workers=10):
+        self.graph = Graph('neo4j://localhost:7687', auth=('neo4j', 'rootrootroot'))
+        self.batch_size = batch_size
+        self.max_workers = max_workers
+        self.thread_positions = {}
+
+
+    def batch_insert_entities(self, entities: list[dict[str, Any]]):
+        def insert(i: int, entities: list[dict[str, Any]]):
+            thread_id = threading.get_ident()
+            position = self.__get_position(thread_id)
+            for entity in tqdm(entities, total=len(entities), position=position, leave=False, desc=f'Batch-{i}'):
+                if 'properties' not in entity:
+                    entity['properties'] = {}
+                node = Node(entity['type'], id=entity['id'], name=entity['name'], **entity['properties'])
+                self.graph.merge(node, entity['type'], 'id')
+
+
+        self.__batch_process(entities, insert)
+
+
+    def batch_insert_relations(self, relations: list[dict[str, str]]):
+        def insert(i: int, relations: list[dict[str, str]]):
+            thread_id = threading.get_ident()
+            position = self.__get_position(thread_id)
+            for relation in tqdm(relations, total=len(relations), position=position, leave=False, desc=f'Batch-{i}'):
+                from_entity_id = relation['from_entity_id']
+                to_entity_id = relation['to_entity_id']
+                from_entity_type = relation['from_entity_type']
+                to_entity_type = relation['to_entity_type']
+                from_node = self.graph.nodes.match(from_entity_type, id=from_entity_id).first()
+                to_node = self.graph.nodes.match(to_entity_type, id=to_entity_id).first()
+                if from_node and to_node:
+                    relationship = Relationship(from_node, relation['type'], to_node)
+                    self.graph.merge(relationship, relation['type'], 'id')
+
+
+        self.__batch_process(relations, insert)
+
+
+    def __batch_process(self, elements, method):
+        total = len(elements)
+        batches = [elements[i:i + self.batch_size] for i in range(0, len(elements), self.batch_size)]
+        batch_num = len(batches)
+        print(f'Batch number: {batch_num}, total: {total}')
+        tasks = {}
+        with futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            for i, batch in enumerate(batches):
+                future = executor.submit(method, i, batch)
+                tasks[future] = batch
+
+        for future in futures.as_completed(tasks):
+            try:
+                future.result()
+            except Exception as e:
+                loguru.logger.exception(e)
+
+
+    def __get_position(self, thread_id):
+        with threading.Lock():
+            if thread_id not in self.thread_positions:
+                self.thread_positions[thread_id] = len(self.thread_positions)
+            return self.thread_positions[thread_id]
+
+
+def insert(filename, type_):
+    db = Database(batch_size=2500, max_workers=15)
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = [json.loads(line) for line in f]
+    if type_ == 'relation':
+        db.batch_insert_relations(data)
+    elif type_ == 'entity':
+        db.batch_insert_entities(data)
+
+
+if __name__ == '__main__':
+    # insert('../output/result_entities.jsonl', 'entity')
+    insert('../output/paper_result_relations.jsonl', 'relation')
--- src/model_extract.py
+++ src/model_extract.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
+import json
+import threading
+import time
+from concurrent import futures
+from pathlib import Path
 from typing import TypeAlias
 
-from zhipuai import ZhipuAI
-from concurrent import futures
+import json_repair
 from loguru import logger
 from tqdm import tqdm
-import time
+from zhipuai import ZhipuAI
 
 Sample: TypeAlias = dict[str, str]
 
@@ -16,7 +20,7 @@ def timer(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        logger.info(f"End {func.__name__} spending {(end - start):2f} seconds")
+        logger.info(f"End {func.__name__} spending {(end - start):.2f} seconds")
         return result
     return wrapper
 
@@ -27,19 +31,17 @@ class Client:
         self.client = ZhipuAI(api_key='ba802c1855ebdba8235bf095766f104e.oAwCWXo3QFwHuAi7')
         self.batch_size = batch_size
         self.max_workers = max_workers
+        self.thread_positions = {}
 
     @timer
     def extract(self, texts: list[Sample]) -> list[Sample]:
         results = []
         texts = [texts[i:i + self.batch_size] for i in range(0, len(texts), self.batch_size)]
-        processing_bars = []
         tasks = {}
         with futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             for i, batch in enumerate(texts):
-                pbar = tqdm(total=len(batch), desc=f'Batch-{i + 1}', position=i, dynamic_ncols=True)
-                processing_bars.append(pbar)
-                future = executor.submit(self.__chat, batch, pbar)
-                tasks[future] = (batch, pbar)
+                future = executor.submit(self.__chat, i, batch)
+                tasks[future] = (i, batch)
 
         for future in futures.as_completed(tasks):
             try:
@@ -47,13 +49,13 @@ class Client:
             except Exception as e:
                 logger.error(e)
 
-        for bar in processing_bars:
-            bar.close()
-
         return results
 
 
-    def __chat(self, batch: list[Sample], pbar) -> list[dict[str, str]]:
+    def __chat(self, i: int, batch: list[Sample]) -> list[dict[str, str]]:
+        thread_id = threading.get_ident()
+        position = self.__get_position(thread_id)
+        pbar = tqdm(total=len(batch), desc=f'Batch-{i + 1}', leave=False, position=position)
         task_texts: dict[str, str] = {}
         for sample in batch:
             task = self.client.chat.asyncCompletions.create(model='GLM-4-Flash', messages=sample['messages'])
@@ -76,8 +78,37 @@ class Client:
                         })
                 time.sleep(2)
 
+        pbar.close()
         return results
 
 
-    def __batch_process(self, batch: list[Sample], pbar) -> list[dict[str, str]]:
-        pass
+    def __get_position(self, thread_id):
+        with threading.Lock():
+            if thread_id not in self.thread_positions:
+                self.thread_positions[thread_id] = len(self.thread_positions)
+            return self.thread_positions[thread_id]
+
+
+def load_data(filename: str):
+    data_dir = Path(__file__).parent.parent / 'data'
+    with open(data_dir / filename, 'r', encoding='utf8') as f:
+        data = [json.loads(line) for line in f]
+        data = [{'id': d['custom_id'], 'messages': d['body']['messages']} for d in data]
+    return data
+
+
+def save(data: list[dict[str, str]], filename: str):
+    output_dir = Path(__file__).parent.parent / 'output'
+    output_dir.mkdir(exist_ok=True)
+    with open(output_dir / filename, 'w', encoding='utf8') as f:
+        for item in data:
+            item['content'] = item['content'].replace('\n', '').strip('```json')
+            item['content'] = json_repair.loads(item['content'])
+            f.write(json.dumps(item, ensure_ascii=False) + '\n')
+
+
+if __name__ == '__main__':
+    data = load_data('data_prompt1.jsonl')
+    client = Client(batch_size=100, max_workers=20)
+    response = client.extract(data)
+    save(response, 'data_prompt1.jsonl')
